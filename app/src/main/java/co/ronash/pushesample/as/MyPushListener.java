848832b83811a7156748117294bc0a0dbc71553f
Akram Shokri <Merka@nobody.invalid>
2016-12-14 16:59:28 +0330

    PU-1742 android studio sample updated

--- app/src/main/java/co/ronash/pushesample/as/MyPushListener.java
+++ app/src/main/java/co/ronash/pushesample/as/MyPushListener.java
@@ -1,6 +1,12 @@
 package co.ronash.pushesample.as;
 
+import android.widget.Toast;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 import co.ronash.pushe.PusheListenerService;
 
@@ -11,10 +17,15 @@ import co.ronash.pushe.PusheListenerService;
  */
 public class MyPushListener extends PusheListenerService {
     @Override
-    public void onMessageReceived(JSONObject message,JSONObject content) {
-        if(message != null && message.length() > 2) {
+    public void onMessageReceived(final JSONObject message, JSONObject content) {
+        if(message != null && message.length() > 0) {
             android.util.Log.i("Pushe", "Custom json Message: " + message.toString());
             //    your code
+            try {
+                String s1 = message.getString("key1");
+            } catch (JSONException e) {
+                android.util.Log.e("","Exception in parsin json" ,e);
+            }
         }
     }
 }
