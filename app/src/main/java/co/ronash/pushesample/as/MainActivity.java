059e5d1b2b0e0953a223349dadfef3b5b4cf2677
Akram Shokri <Merka@Akrams-MacBook-Pro.local>
2017-09-02 12:15:29 +0430

    PU-2332 android studio sample updated to use v1.3.1 of pushe

--- app/src/main/java/co/ronash/pushesample/as/MainActivity.java
+++ app/src/main/java/co/ronash/pushesample/as/MainActivity.java
@@ -2,9 +2,11 @@ package co.ronash.pushesample.as;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 
 import co.ronash.pushe.Pushe;
+import co.ronash.pushe.k.c;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Pushe.initialize(this, true);
+        if(Pushe.isPusheInitialized(this)){
+            Pushe.sendSimpleNotifToUser(this, Pushe.getPusheId(this), "Hi", "It is a notificatin from app to itself");
+            try {
+                Pushe.sendCustomJsonToUser(this, Pushe.getPusheId(this), "{\"key\": \"It is a json from app to itself\"}");
+            } catch (co.ronash.pushe.k.c c) {
+                Log.e("Pushe-AS-Sample", c.getMessage());
+            }
+        }
     }
 }
