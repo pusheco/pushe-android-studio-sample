976ee9ce0098c65500534f01a53b27bb1e6d17c7
mahdi-malv <mmalvandi75@gmail.com>
2019-12-28 16:40:34 +0330

    Feat: Add custom firebase service to handle Pushe and FirebaseMessaging at the same time

---
+++ app/src/main/java/co/ronash/pushesample/as/services/FirebaseCompanion.java
@@ -0,0 +1,52 @@
+package co.ronash.pushesample.as.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import co.ronash.pushe.Pushe;
+
+public class FirebaseCompanion extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        if (Pushe.getFcmHandler(this).onMessageReceived(remoteMessage)) {
+            // Message is for Pushe
+            return;
+        }
+        super.onMessageReceived(remoteMessage);
+
+        // Handle Firebase message
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        Pushe.getFcmHandler(this).onNewToken(s);
+        super.onNewToken(s);
+
+        // Token is refreshed
+    }
+
+    @Override
+    public void onMessageSent(String s) {
+        Pushe.getFcmHandler(this).onMessageSent(s);
+        super.onMessageSent(s);
+
+        // Message sent
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        Pushe.getFcmHandler(this).onDeletedMessages();
+        super.onDeletedMessages();
+
+        // Message was deleted
+    }
+
+    @Override
+    public void onSendError(String s, Exception e) {
+        Pushe.getFcmHandler(this).onSendError(s, e);
+        super.onSendError(s, e);
+
+        // Error sent
+    }
+}
